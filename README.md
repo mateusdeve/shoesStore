7c12c84d80a804eca156d20bcd39a70775bc0cbc
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-05-05 21:38:23 -0300

    initial commit :fire:

---
+++ README.md
@@ -0,0 +1,2 @@
+# Shoes Store
+Um e-commerce Web e mobile desenvolvido com React.js e React Naive utilizando Redux
---
+++ ShoesStore-web/src/pages/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Main() {
+    return <h1>to testando</h1>;
+}
+
+export default Main;
