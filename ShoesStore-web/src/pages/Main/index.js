5f2045b7e541a0fcb1df68c0730a56a05be4c2cb
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-05-06 15:39:38 -0300

    feat: getting data from api from main page

--- ShoesStore-web/src/pages/Main/index.js
+++ ShoesStore-web/src/pages/Main/index.js
@@ -1,94 +1,42 @@
-import React from 'react';
+/* eslint-disable react/state-in-constructor */
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
-export default function Main() {
-    return (
-        <ProductList>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/bnn/l_netshoes/2020-04-23/4108_trio5.jpg"
-                    alt="ok"
-                />
-                <strong>Tenis top do top</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADCIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/bnn/l_netshoes/2020-04-23/4108_trio5.jpg"
-                    alt="ok"
-                />
-                <strong>Tenis top do top</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADCIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/bnn/l_netshoes/2020-04-23/4108_trio5.jpg"
-                    alt="ok"
-                />
-                <strong>Tenis top do top</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADCIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/bnn/l_netshoes/2020-04-23/4108_trio5.jpg"
-                    alt="ok"
-                />
-                <strong>Tenis top do top</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADCIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/bnn/l_netshoes/2020-04-23/4108_trio5.jpg"
-                    alt="ok"
-                />
-                <strong>Tenis top do top</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADCIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/bnn/l_netshoes/2020-04-23/4108_trio5.jpg"
-                    alt="ok"
-                />
-                <strong>Tenis top do top</strong>
-                <span>R$ 129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADCIONAR AO CARRINHO</span>
-                </button>
-            </li>
-        </ProductList>
-    );
+export default class Main extends Component {
+    state = {
+        products: [],
+    };
+
+    async componentDidMount() {
+        const response = await api.get('/products');
+        const data = response.data.map((product) => ({
+            ...product,
+            priceFormated: formatPrice(product.price),
+        }));
+        this.setState({ products: data });
+    }
+
+    render() {
+        const { products } = this.state;
+        return (
+            <ProductList>
+                {products.map((product) => (
+                    <li key={String(product.id)}>
+                        <img src={product.image} alt={product.title} />
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormated} </span>
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#fff" /> 3
+                            </div>
+                            <span>ADCIONAR AO CARRINHO</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        );
+    }
 }
